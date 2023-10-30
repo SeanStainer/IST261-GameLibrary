b3d892a9a1578fca1fda1a0fd90685057112a6f4
SeanStainer <sgs5386@psu.edu>
2023-10-30 16:13:13 -0400

    Changed from eclipse to intelliJ

--- IST261_GameLibraryMain/src/application/Main.java
+++ IST261_GameLibraryMain/src/application/Main.java
@@ -20,29 +20,21 @@ public class Main extends Application
 
 	public void start(Stage primaryStage) 
 	{
-		try 
+		try
 		{
-			/*
-			 * Calls the VideoGameView that presents a Layout
-			 * For all the information inside the Video Games Class
-			 * 
-			 * */
-			
-			//VideoGameView.VideoGameInfo();
-			
 			/*
 			 * Calls the HomeMenuView that presents a main screen
 			 * that contains a menu to choose which layout you want
 			 * to add information into
 			 * 
 			 * */
-			
+
 			HomeView.HomeMenuView();
 			
 		} 
 		catch(Exception e) 
 		{
-			e.printStackTrace();
+			System.out.println("error");
 		}
 	}
 	
